8602b8e1777c74a9abdeecf2b18d86540414da99
Robert Klinger <robert@k1inger.de>
2021-09-23 12:51:45 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+This framework exports the default container used in implementations of the bugfinder-framework.
+
+If you like to use one of these containers make sure to set this package as a peer-dependency of your package!
+
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {Container} from "inversify";
+
+export const localityAContainer = new Container()
+export const localityBContainer = new Container()
+export const quantifierContainer = new Container()
+export const annotatorContainer = new Container()
+export const preprocessingContainer = new Container()
